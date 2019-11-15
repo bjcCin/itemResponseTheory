753152e4998c36aaff532578f817bfaf793cc9d7
Bruno Cavalcanti <bjcc@cin.ufpe.br>
2019-11-15 17:12:48 -0300

    Add questions difficult and habilities

--- CSV_GENERATE.R
+++ CSV_GENERATE.R
@@ -1,12 +1,21 @@
 install.packages("ltm")
 library("ltm")
 
-colnumber <- ncol(X3pt_dichotomic_responses)
-data <- X3pt_dichotomic_responses[2:colnumber]
+sheet_file <-  X3pt_dichotomic_responses
+colnumber <- ncol(sheet_file)
+data <- sheet_file[2:colnumber]
 
 ML3 <- tpm(data, IRT.param = TRUE)
-pp_map <- factor.scores(ML3, method = "EB", resp.patterns = data)
+questions_difficult <- coef(ML3)
+frame <- data.frame(questions_difficult)
 
-tmp1 <- data.frame(MAP = pp_map$score.dat$z1)
+write.csv(frame,"Desktop//TRI//itemResponseTheory//QUESTIONS//pt_3.csv", row.names = FALSE)
 
-write.csv(tmp1,"Desktop//TRI//itemResponseTheory//ABILITY//abilities.csv", row.names = FALSE)
+
+# pp_map <- factor.scores(ML3, method = "EB", resp.patterns = data)
+
+# tmp1 <- data.frame(COD = sheet_file[1]  ,MAP = pp_map$score.dat$z1)
+
+# write.csv(tmp1,"Desktop//TRI//itemResponseTheory//ABILITY//abilities.csv", row.names = FALSE)
+
+
